f2cb63ec3fd5340d8e97ce1c43c2982bb89a1609
Christoph Ebert <ebert86@gmail.com>
2013-03-06 20:10:46 +0100

    Copy apache2.conf to the right location

--- config/dist/distfiles.copy.rb
+++ config/dist/distfiles.copy.rb
@@ -8,6 +8,10 @@ end;
 if !File.exists? File.dirname(__FILE__) + "/../sharedfolders.rb"
   FileUtils.cp(File.dirname(__FILE__) + "/sharedfolders.rb", File.dirname(__FILE__) + "/../sharedfolders.rb");
 end;
+if !File.exists? File.dirname(__FILE__) + "/../apache2.conf.erb"
+  FileUtils.cp(File.dirname(__FILE__) + "/apache2.conf.erb", File.dirname(__FILE__) + "/../apache2.conf.erb");
+end;
 
 # copy templates to the right location
 FileUtils.cp(File.dirname(__FILE__) + "/../vhost.conf.erb", File.dirname(__FILE__) + "/../../cookbooks/php/templates/default/vhost.conf.erb");
+FileUtils.cp(File.dirname(__FILE__) + "/../apache2.conf.erb", File.dirname(__FILE__) + "/../../cookbooks/php/templates/default/apache2.conf.erb");
