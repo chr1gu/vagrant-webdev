11ddfea3f11e06d4825f5557a63d6be9d86c396f
Christoph Ebert <ebert86@gmail.com>
2013-03-06 18:52:41 +0100

    Add example shared folder

--- config/dist/sharedfolders.rb
+++ config/dist/sharedfolders.rb
@@ -1,7 +1,4 @@
 # shared folders for your projects
 Vagrant::Config.run do |config|
-  config.vm.share_folder "antidoping.lo", "/antidoping.lo", "/Users/chrigu/Sites/liip/antidoping", :nfs => true
-  config.vm.share_folder "chatrooms.lo", "/chatrooms.lo", "/Users/chrigu/Sites/private/Chatrooms", :nfs => true
-  config.vm.share_folder "friend-battle.lo", "/friend-battle.lo", "/Users/chrigu/Sites/liip/friend-battle", :nfs => true
-  config.vm.share_folder "app.etite.lo", "/app.etite.lo", "/Users/chrigu/Dropbox/etite/projects/private_work/app.etite.ch", :nfs => true
+#  config.vm.share_folder "example.lo", "/example.lo", "~/Sites/example.lo", :nfs => true
 end
